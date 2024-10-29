9dd25517dc9f24745df2f47e7f5f9ec1b6eae0fe
Daniel Tenorio <danyjr@outlook.es>
2024-10-29 08:52:05 -0600

    Add project files.

---
+++ Prueba3/REGISTRO.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="REGISTRO.aspx.cs" Inherits="Prueba3.REGISTRO" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            BIENVENIDO A LA SECCIÓNDE REGISTRO
+            <br />
+            <br />
+            BUSCAR POR ID: <asp:TextBox ID="txtNombreBuscar" runat="server"></asp:TextBox>
+            &nbsp;<asp:Button ID="btnBuscar" runat="server" Text="BUSCAR" OnClick="BtnBuscar_Click"/>
+            &nbsp;<asp:Button ID="btnEliminar" runat="server" Text="ELIMINAR" OnClick="BtnEliminar_Click"/>
+            <asp:Button ID="btnModificar" runat="server" Text="MODIFICAR" OnClick="BtnModificar_Click"/>
+            <br />
+            <br />
+            ID USUARIO: <asp:TextBox ID="id_user" runat="server" ReadOnly="True" ></asp:TextBox>
+            <br />
+            <br />
+            NOMBRE:<asp:TextBox ID="nombre" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            EMAIL:<asp:TextBox ID="email" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            EDAD:<asp:TextBox ID="edad" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            USUARIO:<asp:TextBox ID="nom_user" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            PASSWORD:<asp:TextBox ID="pass" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="btnRegistrar" runat="server" Text="REGISTRAR" OnClick="Reg_Click"/>
+            <br />
+<br />
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataSourceID="SqlDataSource1" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+                <Columns>
+                    <asp:BoundField DataField="id_user" HeaderText="id_user" InsertVisible="False" ReadOnly="True" SortExpression="id_user" />
+                    <asp:BoundField DataField="nombre" HeaderText="nombre" SortExpression="nombre" />
+                    <asp:BoundField DataField="email" HeaderText="email" SortExpression="email" />
+                    <asp:BoundField DataField="edad" HeaderText="edad" SortExpression="edad" />
+                    <asp:BoundField DataField="nom_user" HeaderText="nom_user" SortExpression="nom_user" />
+                    <asp:BoundField DataField="pass" HeaderText="pass" SortExpression="pass" />
+                </Columns>
+            </asp:GridView>
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="Data Source=DANY-LAPTOP;Initial Catalog=bdprueba;Integrated Security=True;Encrypt=False;" SelectCommand="SELECT * FROM [usuarios]"></asp:SqlDataSource>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Prueba3/REGISTRO.aspx.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Description;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Prueba3
+{
+    public partial class REGISTRO : System.Web.UI.Page
+    {
+
+        String strConexion; // Cadena de conexión
+        String strSQL; // Cadena de instrucción de comando en SQ
+        SqlConnection conexionSQL;
+
+        //Se utiliza para ejecutar 
+        //procedimientos almacenados
+        SqlCommand comandoSQL;
+        //Representa una cache de memoria interna de datos 
+        DataSet datos;
+        //variable de tipo entero que muestra el resultado
+
+        //objeto que verifica si existe un registro deseado o no
+        Object existe;
+
+        Boolean buscado = false;
+        string conexion;
+        String mensaje, mostrarMsg;
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void BtnEliminar_Click(object sender, EventArgs e)
+        {
+            
+
+
+            // Cadena de caracteres a configurar para la conexión
+            strConexion = "Data Source= (local);" + // Nombre del servidor
+                          "Initial Catalog=bdprueba;" + // Nombre de la Base de Datos
+                          "Integrated Security=True;"; // Configuración para la seguridad integrada
+
+            // Este es otro ejemplo para la configuración en cadena para la conexión 
+
+
+            // Comando en SQL para dar de alta en la tabla
+            strSQL = "exec Bajas_Clientes " + (txtNombreBuscar.Text);
+
+            // Instanciamos el objeto conexionSQL para empezar la conexión de forma física
+            conexionSQL = new System.Data.SqlClient.SqlConnection(strConexion);
+
+            try
+            {
+                conexionSQL.Open(); // Abrir la conexion
+
+                // Instanciamos el objeto comandoSQL para instroducir la cadena de comando acorde
+                // a la cadena de conexión
+                comandoSQL = new System.Data.SqlClient.SqlCommand(strSQL, conexionSQL);
+                comandoSQL.ExecuteNonQuery();
+
+                // Ejecuta el query sin hacer una consulta 
+
+                mensaje = "Se elimino El registro";
+                mostrarMsg = "<script language='javascript'>alert('" + mensaje + "');<" + "/script>";
+                Response.Write(mostrarMsg);
+            }
+            catch (Exception ex)
+            {
+
+                mensaje = "Error al Eliminar: ";
+                mostrarMsg = "<script language='javascript'>alert('" + mensaje + "');<" + "/script>";
+                Response.Write(mostrarMsg);
+            }
+            finally
+            {
+                conexionSQL.Close();
+                Response.Redirect(Request.RawUrl);
+            }
+            //limpia todos los campos despues de que agrega un nuevo registro
+
+            id_user.Text = "";
+            nombre.Text = "";
+            email.Text = "";
+            edad.Text = "";
+            nom_user.Text = "";
+            pass.Text = "";
+            txtNombreBuscar.Text = "";
+
+
+
+        }
+    
+
+    public Boolean BuscarNombre(string id_user) { 
+
+            // Declcaramos cadenas, una para la cadena de conexión y otra para la cadena de comandos en SQL
+
+
+
+            // Cadena de caracteres a configurar para la conexión
+            conexion = "Data Source= (local);" + // Nombre del servidor
+                          "Initial Catalog=bdprueba;" + // Nombre de la Base de Datos
+                          "Integrated Security=True;"; // Configuración para la seguridad integrada
+                                                       //inicializamos la conexion
+            conexionSQL = new SqlConnection(conexion);
+            //Abrimos la conexion
+            conexionSQL.Open();
+            //inicializamos los comandos en sql
+            //para ejecutar los procedimientos almaceados
+            comandoSQL = new SqlCommand();
+
+            comandoSQL.CommandType = CommandType.Text;
+
+            //comando en SQL para buscar y mostrar datos de una tabla
+            comandoSQL.CommandText = "SELECT * FROM usuarios WHERE id_user='" + id_user + "'";
+            comandoSQL.Connection = conexionSQL;
+
+            //ejecuta 
+            existe = new Object();
+            existe = comandoSQL.ExecuteScalar();
+
+            //cerramos la conexion
+            conexionSQL.Close();
+
+            if (existe != null)
+                return true;
+            else
+                return false;
+        }
+
+        protected void BtnBuscar_Click(object sender, EventArgs e)
+        {
+            buscado = BuscarNombre(txtNombreBuscar.Text);
+            if (buscado == true)
+            {
+                try
+                {
+                    conexionSQL = new SqlConnection(conexion);
+                    conexionSQL.Open();
+
+                    comandoSQL = new SqlCommand();
+                    comandoSQL.CommandType = CommandType.Text;
+                    comandoSQL.CommandText = "SELECT * FROM usuarios WHERE id_user ='" +
+                                      txtNombreBuscar.Text + "'";
+                    comandoSQL.Connection = conexionSQL;
+
+                    datos = new DataSet();
+                    SqlDataAdapter adaptador = new SqlDataAdapter(comandoSQL);
+                    adaptador.Fill(datos, "usuarios");
+
+                    if (datos.Tables["usuarios"].Rows.Count > 0)
+                    {
+                        mensaje = "USUARIO ENCONTRADO";
+                        mostrarMsg = "<script language='javascript'>alert('" + mensaje + "');<" + "/script>";
+                        Response.Write(mostrarMsg);
+
+                        id_user.Text = Convert.ToString(datos.Tables["usuarios"].Rows[0]["id_user"]);
+                        nombre.Text = Convert.ToString(datos.Tables["usuarios"].Rows[0]["nombre"]);
+                        email.Text = Convert.ToString(datos.Tables["usuarios"].Rows[0]["email"]);
+                        edad.Text = Convert.ToString(datos.Tables["usuarios"].Rows[0]["edad"]);
+                        nom_user.Text = Convert.ToString(datos.Tables["usuarios"].Rows[0]["nom_user"]);
+                        pass.Text = Convert.ToString(datos.Tables["usuarios"].Rows[0]["pass"]);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+                finally
+                {
+                    conexionSQL.Close();
+                }
+            }
+            else
+                //MessageBox.Show("El CLIENTE no existe en la base de datos");
+                mensaje = "El USUARIO no existe en la base de datos";
+            mostrarMsg = "<script language='javascript'>alert('" + mensaje + "');<" + "/script>";
+            Response.Write(mostrarMsg);
+        }
+
+        protected void BtnModificar_Click(object sender, EventArgs e)
+        {
+            string mensaje = " ";
+            string mostrarMsg = " ";
+
+            String strConexion; // Cadena de conexión
+            String strSQL; // Cadena de instrucción de comando en SQL
+
+            // Declaramos dos objetos, uno para la conexión y otro para el comando en SQL
+            System.Data.SqlClient.SqlConnection conexionSQL; // objeto llamado conexionSQL
+            System.Data.SqlClient.SqlCommand comandoSQL; // objeto para utilizar el comando en SQL
+
+            // Cadena de caracteres a configurar para la conexión
+            strConexion = "Data Source=(local);" + // Nombre del servidor
+                          "Initial Catalog=bdprueba;" + // Nombre de la Base de Datos
+                          "Integrated Security=True;" ; // Configuración para la seguridad integrada
+
+            // Este es otro ejemplo para la configuración en cadena para la conexión 
+
+
+            // Comando en SQL para dar de alta en la tabla
+            strSQL = "exec Cambios_Usuarios '" + nombre.Text +
+                "' , '" + email.Text + "' , '" + edad.Text +
+                "' , '" + nom_user.Text + "' , '" + pass.Text +
+                "' , '" + txtNombreBuscar.Text + "'";
+
+            // Instanciamos el objeto conexionSQL para empezar la conexión de forma física
+            conexionSQL = new System.Data.SqlClient.SqlConnection(strConexion);
+
+            try
+            {
+                conexionSQL.Open(); // Abrir la conexion
+
+                // Instanciamos el objeto comandoSQL para instroducir la cadena de comando acorde
+                // a la cadena de conexión
+                comandoSQL = new System.Data.SqlClient.SqlCommand(strSQL, conexionSQL);
+                comandoSQL.ExecuteNonQuery();
+
+                // Ejecuta el query sin hacer una consulta 
+                mensaje = "Se Modifico el registro";
+                mostrarMsg = "<script language='javascript'>alert('" + mensaje + "');<" + "/script>";
+                Response.Write(mostrarMsg);
+
+
+            }
+            catch (Exception ex)
+            {
+
+                mensaje = "Error al Modificar: " + ex.Message;
+                mostrarMsg = "<script language='javascript'>alert('" + mensaje + "');<" + "/script>";
+                Response.Write(mostrarMsg);
+
+            }
+            finally
+            {
+                conexionSQL.Close();
+                Response.Redirect(Request.RawUrl);
+
+
+
+            }
+            //limpia todos los campos despues de que agrega un nuevo registro
+
+            id_user.Text = "";
+            nombre.Text = "";
+            email.Text = "";
+            edad.Text = "";
+            nom_user.Text = "";
+            pass.Text = "";
+            txtNombreBuscar.Text = "";
+        }
+
+        public Boolean ValidarCampos()
+        {
+
+
+            if (nombre.Text == "")
+            {
+                mensaje = "El nombre no debe de estar vacio";
+                mostrarMsg = "<script language='javascript'>alert('" + mensaje + "');<" + "/script>";
+                Response.Write(mostrarMsg);
+                return false;
+            }
+
+            if (email.Text == "")
+            {
+                mensaje = "El correo no debe de estar vacio";
+                mostrarMsg = "<script language='javascript'>alert('" + mensaje + "');<" + "/script>";
+                Response.Write(mostrarMsg);
+                return false;
+            }
+            if (edad.Text == "")
+            {
+                mensaje = "La edad no debe de estar vacio";
+                mostrarMsg = "<script language='javascript'>alert('" + mensaje + "');<" + "/script>";
+                Response.Write(mostrarMsg);
+                return false;
+            }
+            if (nom_user.Text == "")
+            {
+                mensaje = "El usuario no debe de estar vacio";
+                mostrarMsg = "<script language='javascript'>alert('" + mensaje + "');<" + "/script>";
+                Response.Write(mostrarMsg);
+                return false;
+
+
+            }
+            if (pass.Text == "")
+            {
+                mensaje = "la contraseña no debe de estar vacia";
+                mostrarMsg = "<script language='javascript'>alert('" + mensaje + "');<" + "/script>";
+                Response.Write(mostrarMsg);
+                return false;
+
+
+            }
+            else
+                return true;
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SqlDataSource2_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+
+        }
+
+        protected void Reg_Click(object sender, EventArgs e)
+        {
+            Boolean valido;
+
+            valido = ValidarCampos();
+            if (valido == true)
+            {
+
+
+
+                // Cadena de caracteres a configurar para la conexión
+                strConexion = "Data Source= (local);" + // Nombre del servidor
+                              "Initial Catalog=bdprueba;" + // Nombre de la Base de Datos
+                              "Integrated Security=True;"; // Configuración para la seguridad integrada
+
+                // Este es otro ejemplo para la configuración en cadena para la conexión
+                /* 
+                strConexion = "Data Source=TELMEX-VAIO;" + 
+                              "Initial Catalog=EjemploABCC;" +
+                              "User=sa;" +
+                              "Password=;";
+                 */
+
+                // Comando en SQL para dar de alta en la tabla
+                strSQL = "INSERT INTO usuarios " +
+                        "VALUES('" + nombre.Text + "','" + email.Text + "','" +
+                        edad.Text + "','" + nom_user.Text + "','" + pass.Text + "')";
+
+                // Instanciamos el objeto conexionSQL para empezar la conexión de forma física
+                conexionSQL = new System.Data.SqlClient.SqlConnection(strConexion);
+
+                try
+                {
+                    conexionSQL.Open(); // Abrir la conexion
+
+                    // Instanciamos el objeto comandoSQL para instroducir la cadena de comando acorde
+                    // a la cadena de conexión
+                    comandoSQL = new System.Data.SqlClient.SqlCommand(strSQL, conexionSQL);
+                    comandoSQL.ExecuteNonQuery();
+                    mensaje = "Registro Exitoso. ";
+                    mostrarMsg = "<script language='javascript'>alert('" + mensaje + "');<" + "/script>";
+                    Response.Write(mostrarMsg);
+
+
+                }
+                catch (Exception ex)
+                {
+                    mensaje = "Ocurrió un error. ";
+                    mostrarMsg = "<script language='javascript'>alert('" + mensaje + ex + "');<" + "/script>";
+                    Response.Write(mostrarMsg);
+                }
+                finally
+                {
+                    conexionSQL.Close();
+
+                }
+                //limpia todos los campos despues de que agrega un nuevo registro
+
+
+                nombre.Text = "";
+                email.Text = "";
+                edad.Text = "";
+                nom_user.Text = "";
+                pass.Text = "";
+
+
+            }
+        }
+    }
+}
